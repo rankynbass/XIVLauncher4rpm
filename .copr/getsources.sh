2547b44bc3cce5fe6cc281659535eb535c5dbc17
Rankyn Bass <109918887+rankynbass@users.noreply.github.com>
2022-09-04 18:25:45 -0700

    Bump release to 3a

--- .copr/getsources.sh
+++ .copr/getsources.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 UpstreamTag=6246fde
-DownstreamTag=no-git
+DownstreamTag=1.0.1.0-3a
 xlsource=$(rpmbuild --eval='%_sourcedir')
 curl -L https://github.com/goatcorp/FFXIVQuickLauncher/archive/$UpstreamTag.tar.gz -o $xlsource/FFXIVQuickLauncher-$UpstreamTag.tar.gz
 curl -L https://github.com/rankynbass/XIVLauncher4rpm/archive/$DownstreamTag.tar.gz -o $xlsource/XIVLauncher4rpm-$DownstreamTag.tar.gz
