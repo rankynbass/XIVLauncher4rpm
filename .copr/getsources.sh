be4d336bf9a67cc9345481282cfa1dd2007614a6
Rankyn Bass <rankyn@proton.me>
2022-09-04 18:02:50 -0700

    fix script

--- .copr/getsources.sh
+++ .copr/getsources.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 UpstreamTag=6246fde
-DownstreamTag=1.0.1.0-3
+DownstreamTag=no-git
 xlsource=$(rpmbuild --eval='%_sourcedir')
 curl -L https://github.com/goatcorp/FFXIVQuickLauncher/archive/$UpstreamTag.tar.gz -o $xlsource/FFXIVQuickLauncher-$UpstreamTag.tar.gz
 curl -L https://github.com/rankynbass/XIVLauncher4rpm/archive/$DownstreamTag.tar.gz -o $xlsource/XIVLauncher4rpm-$DownstreamTag.tar.gz
