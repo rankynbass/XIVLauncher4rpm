08b939bb5182b27ec8f468efd7bb596d9db82b2d
Rankyn Bass <rankyn@proton.me>
2022-09-18 09:05:51 -0700

    A few more cleanups for better code.

--- .copr/getsources.sh
+++ .copr/getsources.sh
@@ -4,13 +4,13 @@
 repodir="$(realpath "$(dirname "${BASH_SOURCE[0]}")/../")"
 cd "$repodir" || exit
 
-UpstreamTag=$(awk 'NR==2 {print; exit}' < _version)
-xlver=$(awk 'NR==3 {print; exit}' < _version)
-xlrel=$(awk 'NR==4 {print; exit}' < _version)
-DownstreamTag=$xlver-$xlrel
-xlsource=$(rpmbuild --eval='%_sourcedir')
-source0=$xlsource/FFXIVQuickLauncher-$UpstreamTag.tar.gz
-source1=$xlsource/XIVLauncher4rpm-$DownstreamTag.tar.gz
+UpstreamTag="$(awk 'NR==2 {print; exit}' < _version)"
+xlver="$(awk 'NR==3 {print; exit}' < _version)"
+xlrel="$(awk 'NR==4 {print; exit}' < _version)"
+DownstreamTag="$xlver-$xlrel"
+xlsource="$(rpmbuild --eval='%_sourcedir')"
+source0="$xlsource/FFXIVQuickLauncher-$UpstreamTag.tar.gz"
+source1="$xlsource/XIVLauncher4rpm-$DownstreamTag.tar.gz"
 
 # We could download the correct git tag as a tar.gz file, but we already have all the files here! Why do that?
 # Make a directory, copy the needed files of the repo into the directory, then tar the results.
@@ -20,21 +20,23 @@ tar -czf "$source1" "XIVLauncher4rpm-$DownstreamTag"
 # Delete the temp folder we just made.
 rm -rf "XIVLauncher4rpm-$DownstreamTag"
 
-# Build the _version file. Make sure all lines are correct.
-# This is a "safe" way to do it. This makes sure the version file didn't get any added lines at the end.
-echo "# Line 2: UpstreamTag, Line 3: Version, Line 4: Release. DO NOT DELETE THIS COMMENT LINE" > $xlsource/_version
-echo "$UpstreamTag" >> $xlsource/_version
-echo "$xlver" >> $xlsource/_version
-echo "$xlrel" >> $xlsource/_version
-
 # Now get the latest git from upstream.
 rm -rf FFXIVQuickLauncher
 git clone https://github.com/goatcorp/FFXIVQuickLauncher.git
 cd FFXIVQuickLauncher || exit
 # Get timestamp of current commit.
 xltimestamp="$(date -u -d "@$(git show -s --format=%ct)" +'%y.%m.%d.%H%M')"
-echo "$xltimestamp" >> "$xlsource/_version"
 git archive --format=tar.gz -o "$source0" --prefix=FFXIVQuickLauncher/ HEAD
 # Delete the repo.
 cd "$repodir" || exit
 rm -rf FFXIVQuickLauncher
+
+# Build the _version file. Make sure all lines are correct.
+# This is a "safe" way to do it. This makes sure the version file didn't get any added lines at the end.
+{
+    echo "# Line 2: UpstreamTag, Line 3: Version, Line 4: Release. DO NOT DELETE THIS COMMENT LINE"
+    echo "$UpstreamTag"
+    echo "$xlver"
+    echo "$xlrel"
+    echo "$xltimestamp"
+} > "$xlsource/_version"
