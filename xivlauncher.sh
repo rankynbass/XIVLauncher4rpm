ded72cf726480f318da621c600197096167597c4
Rankyn Bass <rankyn@proton.me>
2022-09-25 17:55:19 -0700

    Redo xivlauncher.sh

--- xivlauncher.sh
+++ xivlauncher.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/bash
-OPENSSL_CONF=/opt/XIVLauncher/openssl_fix.cnf /opt/XIVLauncher/XIVLauncher.Core &
+
+# Define env variables. Top one is mandatory. 
+export OPENSSL_CONF=/opt/XIVLauncher/openssl_fix.cnf
+# export DXVK_FRAME_RATE=60
+# export MANGOHUD=1
+# export MANGOHUD_CONFIGFILE=~/.config/MangoHud/MangoHud.conf
+
+# Steam overlay (doesn't work yet)
+# export LD_PRELOAD="$HOME/.local/share/Steam/ubuntu12_64/gameoverlayrenderer.so"
+
+/opt/XIVLauncher/XIVLauncher.Core &
