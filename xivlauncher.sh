b61943e45dc2ac3a6bde26c4c6923eec1ea4c5c6
Rankyn Bass <rankyn@proton.me>
2022-10-01 14:38:47 -0700

    Improved xivlauncher.sh file

--- xivlauncher.sh
+++ xivlauncher.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/bash
-OPENSSL_CONF=/opt/XIVLauncher-git/openssl_fix.cnf /opt/XIVLauncher-git/XIVLauncher.Core &
+
+# Define env variables. Top one is mandatory. 
+export OPENSSL_CONF=/opt/XIVLauncher/openssl_fix.cnf
+# export DXVK_FRAME_RATE=60
+# export MANGOHUD=1
+# export MANGOHUD_CONFIGFILE=~/.config/MangoHud/MangoHud.conf
+
+/opt/XIVLauncher/XIVLauncher.Core &
