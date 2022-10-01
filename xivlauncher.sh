287d8e02a29973fd7a439ee1af40d9455ccd3ffa
Rankyn Bass <rankyn@proton.me>
2022-10-01 14:40:26 -0700

    point to /opt/XIVLauncher-git

--- xivlauncher.sh
+++ xivlauncher.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
 # Define env variables. Top one is mandatory. 
-export OPENSSL_CONF=/opt/XIVLauncher/openssl_fix.cnf
+export OPENSSL_CONF=/opt/XIVLauncher-git/openssl_fix.cnf
 # export DXVK_FRAME_RATE=60
 # export MANGOHUD=1
 # export MANGOHUD_CONFIGFILE=~/.config/MangoHud/MangoHud.conf
 
-/opt/XIVLauncher/XIVLauncher.Core &
+/opt/XIVLauncher-git/XIVLauncher.Core &
