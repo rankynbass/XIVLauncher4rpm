b51ed1040e1f3a0ec6d9676cab2662b7b6157359
Rankyn Bass <rankyn@proton.me>
2022-10-02 13:30:23 -0700

    Cleanup local.sh

--- .copr/local.sh
+++ .copr/local.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+UpstreamTag=$(awk 'NR==2 {print; exit}' < _version)
+DownstreamTag=$(awk 'NR==3 {print; exit}' < _version)-$(awk 'NR==4 {print; exit}' < _version)
 LocalRepo="$HOME/build/FFXIVQuickLauncher"
-
 xlsource="$(rpmbuild --eval='%_sourcedir')"
 
 # Make sure the script can run properly no matter where it's called from
@@ -10,25 +10,15 @@ repodir="$(realpath "$(dirname "${BASH_SOURCE[0]}")/../")"
 
 # Make tarball from local FFXIVQuickLauncher repo
 cd "$LocalRepo/.." || exit
-xlcommit=local
-xltimestamp="$(date -u +'%y.%m.%d.%H%M')"
-tar -czf "$xlsource/FFXIVQuickLauncher-$xlcommit.tar.gz" --exclude="FFXIVQuickLauncher/.git" "FFXIVQuickLauncher"
+tar -czf "$xlsource/FFXIVQuickLauncher-$UpstreamTag.tar.gz" --exclude="FFXIVQuickLauncher/.git" "FFXIVQuickLauncher"
 
 cd "$repodir" || exit
 # We could download the correct git tag as a tar.gz file, but we already have all the files here! Why do that?
 # Make a directory, copy the needed files of the repo into the directory, then tar the results.
-DownstreamTag="$xltimestamp-utc"
 mkdir -p "XIVLauncher4rpm-$DownstreamTag"
 cp cleanupprofile.sh openssl_fix.cnf xivlauncher.sh XIVLauncher.desktop COPYING "XIVLauncher4rpm-$DownstreamTag/"
 tar -czf "$xlsource/XIVLauncher4rpm-$DownstreamTag.tar.gz" "XIVLauncher4rpm-$DownstreamTag"
 # Delete the temp folder we just made.
 rm -rf "XIVLauncher4rpm-$DownstreamTag"
 
-# Build the _version file. Make sure all lines are correct.
-# This is a "safe" way to do it. This makes sure the version file didn't get any added lines at the end.
-{
-    echo "# Line 2: UpstreamTag, Line 3: Version, Line 4: Release. DO NOT DELETE THIS COMMENT LINE"
-    echo "$xlcommit"
-    echo "$xltimestamp"
-    echo "utc"
-} > "$xlsource/_version"
+cp _version "$xlsource"
