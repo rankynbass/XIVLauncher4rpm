ba0fbca3fd84cfe824752581b5cb199a0625ad0d
Rankyn Bass <rankyn@proton.me>
2022-11-28 16:40:52 -0800

    Scripting and spec file cleanup

--- .copr/getsources.sh
+++ .copr/getsources.sh
@@ -8,8 +8,8 @@ LauncherRepo=$(awk 'NR==4 {print; exit}' < "$repodir/_version")
 LauncherTag=$(awk 'NR==5 {print; exit}' < "$repodir/_version")
 DownstreamTag=$(awk 'NR==6 {print; exit}' < "$repodir/_version")-$(awk 'NR==7 {print; exit}' < "$repodir/_version")
 xlsource=$(rpmbuild --eval='%_sourcedir')
-source0=$xlsource/XIVLauncher.Core-$CoreTag.tar.xz
-source1=$xlsource/XIVLauncher4rpm-$DownstreamTag.tar.xz
+source0=$xlsource/XIVLauncher.Core-$CoreTag.tar.gz
+source1=$xlsource/XIVLauncher4rpm-$DownstreamTag.tar.gz
 workingdir=/tmp/xivlauncher
 
 cd "$repodir" || exit
@@ -20,22 +20,26 @@ then
     # So we'll do a little bash magic to get the folder names. Then we'll extract everything, rename as appropriate,
     # and rebuild a full source tarball that includes the submodule.
     cd "$workingdir" || exit
-    curl -L "$CoreRepo/archive/$CoreTag.tar.gz" -o "$CoreTag.tar.gz"
-    curl -L "$LauncherRepo/archive/$LauncherTag.tar.gz" -o "$LauncherTag.tar.gz"
+    echo "Downloading $CoreTag => xlcore-$CoreTag.tar.gz"
+    curl -L "$CoreRepo/archive/$CoreTag.tar.gz" -o "xlcore-$CoreTag.tar.gz"
+    echo "Downloading $LauncherTag => launcher-$LauncherTag.tar.gz"
+    curl -L "$LauncherRepo/archive/$LauncherTag.tar.gz" -o "launcher-$LauncherTag.tar.gz"
     # These next two lines will get the folder names at the top of the tarball
-    CoreDir=$(tar -tf "$CoreTag.tar.gz" | head -n 1)
-    LauncherDir=$(tar -tf "$LauncherTag.tar.gz" | head -n 1)
+    CoreDir=$(tar -tf "xlcore-$CoreTag.tar.gz" | head -n 1)
+    LauncherDir=$(tar -tf "launcher-$LauncherTag.tar.gz" | head -n 1)
     # Extract XIVLauncher.Core tarball and then rename the folder to XIVLauncher.Core
-    tar -xf "$CoreTag.tar.gz"
+    echo "Extracting xlcore-$CoreTag.tar.gz"
+    tar -xf "xlcore-$CoreTag.tar.gz"
     mv "$CoreDir" XIVLauncher.Core
     # Remove an empty directory that will get in our way
     rmdir XIVLauncher.Core/lib/FFXIVQuickLauncher
     # Extract the FFXIVQuickLauncher tarball to XIVLauncher.Core/lib/, then rename to FFXIVQuickLauncher
-    tar -C XIVLauncher.Core/lib -xf "$LauncherTag.tar.gz"
+    echo "Extracting launcher-$LauncherTag.tar.gz and moving files to lib/FFXIVQuickLauncher"
+    tar -C XIVLauncher.Core/lib -xf "launcher-$LauncherTag.tar.gz"
     mv "XIVLauncher.Core/lib/$LauncherDir" "XIVLauncher.Core/lib/FFXIVQuickLauncher"
     # Create a new, complete tarball.
-    echo "Making tarball for XIVLauncher.Core"
-    tar --totals -I 'xz -v -9' -cf "$source0" XIVLauncher.Core
+    echo "Writing XIVLauncher.Core-$CoreTag.tar.gz"
+    tar -czf "$source0" XIVLauncher.Core
 fi
 cd "$repodir" || exit
 if [ ! -f "$source1" ];
@@ -43,10 +47,10 @@ then
     # We could download the correct git tag as a tar.gz file, but we already have all the files here! Why do that?
     # Make a directory, copy the needed files of the repo into the directory, then tar the results.
     mkdir -p "$workingdir/XIVLauncher4rpm-$DownstreamTag"
-    cp cleanupprofile.sh openssl_fix.cnf xivlauncher.sh XIVLauncher.desktop XIVLauncher-custom.desktop COPYING "$workingdir/XIVLauncher4rpm-$DownstreamTag/"
+    cp CHANGELOG.md cleanupprofile.sh openssl_fix.cnf README.md xivlauncher.sh XIVLauncher.desktop COPYING "$workingdir/XIVLauncher4rpm-$DownstreamTag/"
     cd "$workingdir" || exit
-    echo "Making tarball for XIVLauncher4rpm"
-    tar --totals -I 'xz -v -9' -cf "$source1" "XIVLauncher4rpm-$DownstreamTag"
+    echo "Writing XIVLauncher4rpm-$DownstreamTag.tar.gz"
+    tar -czf "$source1" "XIVLauncher4rpm-$DownstreamTag"
 fi
 cp "$repodir/_version" "$xlsource"
 rm -rf "$workingdir"
--- xivlauncher.sh
+++ xivlauncher.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-
+# Version info
+version=0.6
+xlcore=1.0.2
 # ~/.local/bin is part of the systemd file hierarchy. Fedora and openSUSE both use it.
 bindir=$HOME/.local/bin
-script=xivlauncher-custom.sh
+# Make sure XDG_DATA_HOME is set
+if [[ -z "$XDG_DATA_HOME" ]]; then XDG_DATA_HOME=$HOME/.local/share; fi
 
 # Function to make a new custom script
 makeCustomScript ()
@@ -27,6 +30,22 @@ makeCustomScript ()
     chmod +x "$bindir/$script"
 }
 
+makeDesktop()
+{
+    echo -e "Creating new xivlauncher-$1.desktop file in\n$XDG_DATA_HOME/applications"
+    {
+        echo "[Desktop Entry]"
+        echo "Name=XIVLauncher RPM $1"
+        echo "Comment=XIVLauncher RPM $1 script (~/.local/bin/xivlauncher-$1.sh)"
+        echo "Exec=/usr/bin/xivlauncher $1"
+        echo "Icon=xivlauncher"
+        echo "Terminal=true"
+        echo "Type=Application"
+        echo "Categories=Game;"
+        echo "StartupWMClass=XIVLauncher.Core"
+    } > "$XDG_DATA_HOME/applications/XIVLauncher-$1.desktop"
+}
+
 # Function to run without script
 runDefault()
 {
@@ -45,19 +64,16 @@ runDefault()
 checkCustom()
 {
     echo "Checking $bindir/$script"
-    # Code to check for bad or missing $HOME/.local/bin/xivlauncher-custom.sh
-    if [ -f "$bindir/$script" ];
-    then
+    # Code to check for bad or missing custom script
+    if [ -f "$bindir/$script" ]; then
         # Make it executable if it isn't
-        if [ ! -x "$bindir/$script" ];
-        then
+        if [ ! -x "$bindir/$script" ]; then
             chmod +x "$bindir/$script"
         fi
         # Check the file for bash syntax errors.
         errval=$(bash -n $bindir/$script 2>&1)
         retcode=$?
-        if [ ! $retcode == 0 ];
-        then
+        if [ ! $retcode == 0 ]; then
             # If errors are found, backup the file and create a fresh one.
             rm -f "$bindir/$script.bak"
             mv "$bindir/$script" "$bindir/$script.bak"
@@ -69,47 +85,140 @@ checkCustom()
             # Otherwise, check for the SSL fix.
             echo "File exists. Checking for SSL fix."
             sslfix=$(grep -c 'OPENSSL_CONF=/opt/XIVLauncher/openssl_fix.cnf' "$bindir/$script")
-            if [ "$sslfix" -eq 0 ];
-            then
+            if [ "$sslfix" -eq 0 ]; then
                 # If the OPENSSL_CONF line is not found, insert it.
-                echo "Updating xivlauncher-custom.sh with SSL fix."
+                echo "Updating $script with SSL fix."
                 sed -i '2i\export OPENSSL_CONF=/opt/XIVLauncher/openssl_fix.cnf\' "$bindir/$script"
             else
                 echo "SSL config found."
             fi   
         fi
     else
-        # xivlauncher-custom.sh wasn't found, so create it.
+        # custom script wasn't found, so create it.
         makeCustomScript
+    fi
+}
+
+# Function to make .desktop file
+checkDesktop()
+{
+    desktop=$XDG_DATA_HOME/applications/XIVLauncher-$1.desktop
+    echo "Checking for XIVLauncher-$1.desktop file"
+    if [ ! -f "$desktop" ]; then
+        echo "Desktop file not found. Creating in $XDG_DATA_HOME/applications"
+        makeDesktop "$1"
+    else
+        echo "Desktop file found."
     fi
 }
 
 # Function to run with script
 runCustom()
 {
-    echo "===================== /usr/bin/xivlauncher custom ====================="
-    echo "Running script ~/.local/bin/xivlauncher-custom.sh"
-    echo "If it crashes, please delete it, and run /usr/bin/xivlauncher custom"
+    echo "===================== /usr/bin/xivlauncher $1 ====================="
+    echo "Running script ~/.local/bin/$script"
+    echo "If it crashes, please delete it, and run /usr/bin/xivlauncher $1"
     echo "again. If the problem persists, please file a ticket on"
     echo "https://github.com/rankynbass/XIVLauncher4rpm describing the error,"
-    echo "along with the contents of ~/.local/bin/xivlauncher-custom.sh"
-    echo "===================== /usr/bin/xivlauncher custom ====================="
+    echo "along with the contents of ~/.local/bin/$script.sh"
+    echo "===================== /usr/bin/xivlauncher $1 ====================="
     "$bindir/$script"
 }
 
+deleteScript()
+{
+    if [ -z "$1" ]; then
+        echo -e "Missing script name.\n"
+        helpText
+    fi
+    invalidScriptName "$1"
+    if [ -f "$bindir/xivlauncher-$1.sh" ]; then
+        echo "Deleting $bindir/xivlauncher-$1.sh"
+        rm "$bindir/xivlauncher-$1.sh"
+        echo "Deleting $XDG_DATA_HOME/applications/XIVLauncher-$1.desktop"
+        rm -f "$XDG_DATA_HOME/applications/XIVLauncher-$1.desktop"
+    else
+        echo "$bindir/xivlauncher-$1.sh does not exist"
+        exit 0
+    fi
+}
+
+helpText()
+{
+    echo -e "Usage: xivlauncher [OPTION] <script>"
+    echo -e "xivlauncher is a helper script that will launch XIVLauncher.Core, or create and"
+    echo -e "run a user script at ~/.local/bin/xivlauncher-<script>.sh. This script is user"
+    echo -e "modifiable and can be used to export environment variables or launch other"
+    echo -e "programs.\n"
+    echo -e "No options or script:"
+    echo -e "\tRun XIVLauncher.Core without any custom scripts."
+    echo -e "No options:"
+    echo -e "\tRun ~/.local/bin/xivlauncher-<script>.sh. Create it if it doesn't exist."
+    invalidScriptText
+    echo -e "Options:"
+    echo -e "\t-d script\tdeletes ~/.local/bin/xivlauncher-<script>.sh."
+    echo -e "\t-r script\tdeletes ~/.local/bin/xivlauncher-<script>.sh,\n\t\t\t  then creates a fresh one."
+    echo -e "\t-l, --list\tList custom xivlauncher scripts."
+    echo -e "\t-h, --help\tgive this help list"
+    echo -e "\t-v, --version\tprint version information"
+    exit 0
+}
+
+invalidScriptName()
+{
+    if [[ "$1" == *@(\\|/|-|\|)* ]]; then
+        echo -e "Invalid script name: $1\n"
+        echo -e "Usage: xivlauncher [-d,-r] <script>"
+        invalidScriptText
+        exit 0
+    fi
+}
+
+invalidScriptText()
+{
+    echo -e "\t<script> should be a single word. Don't use special characters, spaces,"
+    echo -e "\tdashes or slashes. Underscores are okay."
+}
+
 # Body of script
-if [ -z "$1" ];
-then
+if [ -z "$1" ]; then
     runDefault
 else
-    case $1 in
-        "CUSTOM" | "custom" | "Custom" )
-            checkCustom
-            runCustom
-        ;;
-        * )
-            echo "Error: Option '$1' is not valid."
-            runDefault
-        ;;
+    case "$1" in 
+    -d)
+        deleteScript "$2"
+        echo ""
+        exit 0;;
+    -r)
+        deleteScript "$2"
+        shift
+        echo -e " Now recreating\nxivlauncher-$1...\n";;
+    -h | --help)
+        helpText;;
+    -l | --list)
+        ls "$bindir"/xivlauncher-*.sh
+        exit 0;;
+    -v | --version)
+        echo -e "xivlauncher helper script $version"
+        echo -e "Copyright (C) 2022 Rankyn Bass"
+        echo -e "License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>."
+        echo -e "This is free software: you are free to change and redistribute it."
+        echo -e "There is NO WARRANTY, to the extent permitted by law.\n"
+        echo -e "This is a bash script. It should be safe, but you should always check before"
+        echo -e "running scripts off the internet. This script is not provided by the official"
+        echo -e "XIVLauncher development team; it is part of the COPR RPM release.\n"
+        echo -e "For use with XIVLauncher.Core $xlcore"
+        exit 0;;
+    *)
+        option=${1:0:1}
+        if [ "$option" = "-" ]; then
+            echo -e "Invalid option: $1\n"
+            helpText
+        fi
+        invalidScriptName "$1";;
     esac
+    script="xivlauncher-$1.sh"
+    checkCustom
+    checkDesktop "$1"
+    runCustom "$1"
 fi
