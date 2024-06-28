27e41b4a0a9e96e4de71fd97cbc2e00021446bbc
Rankyn Bass <rankyn@proton.me>
2024-06-27 20:25:39 -0700

    Forgot to stage all changes

--- .copr/getsource.sh
+++ .copr/getsource.sh
@@ -7,31 +7,31 @@ CoreRepo=$(awk 'NR==2 {print; exit}' < "$repodir/_version")
 CoreTag=$(awk 'NR==3 {print; exit}' < "$repodir/_version")
 Release=$(awk 'NR==4 {print; exit}' < "$repodir/_version")
 xlsource=$(rpmbuild --eval='%_sourcedir')
-source0="$xlsource/XIVLauncher.Core-$CoreTag.tar.gz"
-source1=$xlsource/XIVLauncher4rpm-$CoreTag-$Release.tar.gz
+source0="$xlsource/XIVLauncher-RB-$CoreTag.tar.gz"
+source1=$xlsource/XIVLauncher4rpm-rb-v$CoreTag-$Release.tar.gz
 workingdir=/tmp/xivlauncher
 
 mkdir -p "$workingdir"
-rm -rf "$workingdir"/XIVLauncher.Core
-rm -rf "$workingdir"/XIVLauncher4rpm-$CoreTag-$Release
+rm -rf "$workingdir"/XIVLauncher-RB
+rm -rf "$workingdir"/XIVLauncher4rpm
 rm $source0
 rm $source1
 
 cd "$workingdir" || exit
 echo "Cloning $CoreTag from git repository $CoreRepo"
-git clone "$CoreRepo" -b $CoreTag --recurse-submodules
-cd XIVLauncher.Core || exit
+git clone "$CoreRepo" -b rb-v$CoreTag --recurse-submodules XIVLauncher-RB
+cd XIVLauncher-RB || exit
 Hash=$(git rev-parse --short HEAD)
 cd "$workingdir" || exit
-echo "Creating XIVLauncher.Core-$CoreTag.tar.gz"
-tar --exclude=".*" -czf "$source0" XIVLauncher.Core
+echo "Creating XIVLauncher-RB-$CoreTag.tar.gz"
+tar --exclude=".*" -czf "$source0" XIVLauncher-RB
 
 cd "$repodir" || exit
-mkdir -p "$workingdir/XIVLauncher4rpm-$CoreTag-$Release"
-cp CHANGELOG.md README.md xivlauncher.sh XIVLauncher.desktop COPYING "$workingdir/XIVLauncher4rpm-$CoreTag-$Release/"
+mkdir -p "$workingdir/XIVLauncher4rpm"
+cp CHANGELOG.md README.md xivlauncher.sh XIVLauncher.desktop COPYING "$workingdir/XIVLauncher4rpm/"
 cd "$workingdir" || exit
-echo "Creating XIVLauncher4rpm-$CoreTag-$Release.tar.gz"
-tar -czf "$source1" "XIVLauncher4rpm-$CoreTag-$Release"
+echo "Creating XIVLauncher4rpm-rb-v$CoreTag-$Release.tar.gz"
+tar -czf "$source1" "XIVLauncher4rpm"
 
 cat > "$xlsource/_version" << EOF
 $PkgName
